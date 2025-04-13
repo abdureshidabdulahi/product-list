fa51d801461a5deb641cc7db912d976c8c417902
abdureshidabdulahi <abdureshidabdulahi@users.noreply.github.com>
2025-04-13 19:56:33 +0300

    project added

---
+++ productlist.js
@@ -0,0 +1,12 @@
+ 
+ function changeStyle(){
+      document.getElementById('addcart').classList.toggle('newstyle');
+      document.getElementsByClassName('fa-cart-shopping').style.display ='none'
+      document.getElementsByClassName('add').style.display = 'none'
+
+      // let calculate = () =>{
+      //       let a;
+      //       a++;
+
+      // }
+ }
