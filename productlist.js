e1ef5411ef4e5a3bc15241ace766c87c257f82bb
abdureshidabdulahi <abdureshidabdulahi@users.noreply.github.com>
2025-05-05 16:22:02 +0300

    update on js

--- productlist.js
+++ productlist.js
@@ -1,13 +1,29 @@
 let addToCart = function(){
+
      let button = document.querySelector('.image1 button');
      button.addEventListener('click', ()=>{
-          button.classList.toggle('buttons');
-          button.classList.toggle('plu-min');
-          add.classList.toggle('buttons');
-          shopingCart.classList.toggle('buttons')
-
-
-
+          if(!button.classList.contains('buttons')){
+               button.classList.add('buttons')
+               document.querySelector('.add').innerHTML = '1'
+          }
+         
      })
 }
 addToCart();
+document.querySelector('.plu-min').addEventListener('click', incrementFunction)
+let incrementFunction = function(){
+     let order = 1;
+     order++
+      document.querySelector('.add').innerHTML = 'order'
+
+} 
+
+
+
+
+
+
+// button.classList.toggle('buttons');
+// button.classList.toggle('plu-min');
+// add.classList.toggle('buttons');
+// shopingCart.classList.toggle('buttons')
