a0d5294c8d127b9fedd5df3367923f24439d1d3e
abdureshidabdulahi <abdureshidabdulahi@users.noreply.github.com>
2025-05-06 23:17:33 +0300

    update

--- productlist.js
+++ productlist.js
@@ -1,35 +1,32 @@
-let addToCart = function(){
+ 
+let button = document.querySelectorAll('button');
 
-     let button = document.querySelector('.image1 button');
-     button.addEventListener('click', ()=>{
-          if(!button.classList.contains('buttons')){
-               let order = 1;
-               button.classList.add('buttons')
+ let order  = 1;
+ let buttonOneFunc = function(){
+ button[0].addEventListener('click', ()=>{
+     if(!button[0].classList.contains('buttons')){
+          button[0].classList.add('buttons');
+          document.querySelector('.add').innerHTML = order
+     }
+ })
+ }
+ buttonOneFunc();
+
+ document.querySelector('.plus').addEventListener('click', ()=>{
+     function increment(){
+          order++ ;
+          document.querySelector('.add').innerHTML = order
+      }
+      increment();
+ })
+
+ document.querySelector('.minus').addEventListener('click', ()=>{
+     function decrement(){
+          if(order > 0){
+               order-- ;
                document.querySelector('.add').innerHTML = order
           }
-         
-     })
-}
-addToCart();
-let order = 1;
-let incrementFunction = function(){
-     
-     order++
-      document.querySelector('.add').innerHTML = order
-
-} 
-document.querySelector('.plus').addEventListener('click', incrementFunction)
-let decrementFunction = function(){
-     if(order>0){
-          order--
-     }
-     document.querySelector('.add').innerHTML = order;
-     
-
-}
-document.querySelector('.minus').addEventListener('click', decrementFunction)
-
-
-
-
- 
+          
+      }
+      decrement();
+ })
